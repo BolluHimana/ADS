87a0d83ae03fc153c6c268e5dc3fe77e4b0dafe3
BolluHimana <bolluhimana@msitprogram.net>
2019-10-30 18:19:31 +0530

    done!!

--- Day-3/Assignmnet-4/Solution.java
+++ Day-3/Assignmnet-4/Solution.java
@@ -1,7 +1,17 @@
 class Solution{
 	public Team[] sort(Team[] teams){
-		
-		return teams;
+			for(int i=0;i<teams.length-1;i++){
+				int min=i;
+				for(int j=i+1;j<teams.length;j++){
+					if(teams[j].compareTo(teams[min])==1){
+						min=j;
+					}
+				}
+				Team temp=teams[i];
+				teams[i]=teams[min];
+				teams[min]=temp;
+				}
+			return teams;
 	}
 }
 class Team implements Comparable<Team> {
@@ -18,5 +28,30 @@ class Team implements Comparable<Team> {
 	public String toString(){
 		//retrun all the attributes as a string but appending with ", "
         return "";
-    }
+	}
+	public int compareTo(Team that){
+		if(this.noOfWins > that.noOfWins){
+			return 1;
+		}
+		else if(this.noOfWins < that.noOfWins){
+			return -1;
+		}
+		else{
+			if(this.noOfLosses > that.noOfLosses){
+				return -1;
+			}
+			else if(this.noOfLosses < that.noOfLosses){
+				return 1;
+			}
+			else{
+			if(this.noOfDraws > that.noOfDraws){
+				return 1;
+			}
+			else if(this.noOfDraws < that.noOfDraws){
+				return -1;
+			}
+		}
+		}
+		return 0;
+	}
 }
