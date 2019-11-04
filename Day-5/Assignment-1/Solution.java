a122f8a30d63a0d3ce9c82c28b60f2a00d043d4d
BolluHimana <bolluhimana@msitprogram.net>
2019-11-04 18:37:54 +0530

    done!!

---
+++ Day-5/Assignment-1/Solution.java
@@ -0,0 +1,17 @@
+class Solution{
+	public static boolean isMinHeap(double[] arr){
+		if(arr.length<=0){
+			return false;
+		}
+		for (int i=arr.length-1;i>=0;i--){
+			if(arr.length>i/2){
+				if(arr[i/2]>arr[i] && arr[i/2]>arr[i]){
+				return false;
+				}
+			
+			}
+		
+		}
+		return true;
+	}
+}
---
+++ Day-5/Assignment-2/Solution.java
@@ -0,0 +1,5 @@
+class Solution{
+	public static double[] dynamicMedian(double[] arr){
+		return arr;
+	}
+}
