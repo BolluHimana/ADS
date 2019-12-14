67561299e78e02935b46d54e598290940c32ed28
BolluHimana <bolluhimana@msitprogram.net>
2019-12-14 11:47:40 +0530

    ok!!

--- Dec/Tstacks.java
+++ Dec/Tstacks.java
@@ -1,29 +1,43 @@
 import java.util.*;
 class Tstacks{
-    int size;
     static Stack<Integer> s1 = new Stack<Integer>();  
     static Stack<Integer> s2 = new Stack<Integer>();
-
     public int Dequeue(){
+        int c=0;
         while(!s1.isEmpty()){
             s2.push(s1.pop());
+            c=c+1;
+        if(c>0){
+        int a = s2.pop();
+        return a;
         }
-        int a=s2.pop();
+    }
         while(!s2.isEmpty()){
             s1.push(s2.pop());
-        }
-        return a;
+            int a = s1.pop();
+            return a;
+
     }
+    return 0;
+}
     public static void main(String[] args){
         Tstacks ts = new Tstacks();
         s1.push(25);
-        System.out.println(ts.Dequeue());
         s1.push(75);
         s1.push(15); 
         s1.push(3);
         s1.push(9);
-        System.out.println(ts.Dequeue());
-        System.out.println(ts.Dequeue());
+        s2.push(10);
+        s2.push(55);
+        s2.push(95);
+        System.out.print(ts.Dequeue()+" ");
+        System.out.print(ts.Dequeue()+" ");
+        System.out.print(ts.Dequeue()+" ");
+        System.out.print(ts.Dequeue()+" ");
+        System.out.print(ts.Dequeue()+" ");
+        System.out.print(ts.Dequeue()+" ");
+        System.out.print(ts.Dequeue()+" ");
+        System.out.print(ts.Dequeue()+" ");
     }
 
 }
